66649cdaaeccafc0b8b048e552618dec2d299b0b
Gentian <gentian.lamcaj@outlook.com>
2022-09-01 18:47:04 -0400

    modified wine class and added prod

--- src/Wine.java
+++ src/Wine.java
@@ -2,6 +2,11 @@ public class Wine{
     public static void main(String[] args) {
         System.out.println("wine");
 
+        int k=5;
+        int l=10;
+        int prod=k*l;
+        System.out.println(prod);
+
     }
   //wine the best wine
   //this is a change
