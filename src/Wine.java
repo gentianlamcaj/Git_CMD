12ff161edca249317dd72b74ca876d36e83e7a37
gentianlamcaj <107499279+gentianlamcaj@users.noreply.github.com>
2022-09-01 18:45:10 -0400

    Update Wine.java

--- src/Wine.java
+++ src/Wine.java
@@ -1,6 +1,10 @@
 public class Wine{
     public static void main(String[] args) {
         System.out.println("wine");
+         int a=5;
+        int b=6;
+        int sum=a+b;
+        System.out.println(sum);
 
     }
   //wine the best wine
