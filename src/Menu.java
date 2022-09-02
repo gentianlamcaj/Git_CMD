375e1b7a8b82ec0a4482af2f146a8e09e7357a2b
Gentian <gentian.lamcaj@outlook.com>
2022-09-01 21:10:00 -0400

    test

--- src/Menu.java
+++ src/Menu.java
@@ -1,21 +1,15 @@
 
 public class Menu{
   //test test
-<<<<<<< Updated upstream
-  
-  
-  
-  
-  
-  
+
   //all done
   
-=======
+
   public static void main(String[] args) {
       int b=5;
       int c=7;
       int diff=b-c;
       System.out.println(diff);
   }
->>>>>>> Stashed changes
+
 }
